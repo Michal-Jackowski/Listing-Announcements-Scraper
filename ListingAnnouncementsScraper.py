bb8339f5eda99c9da91151bf1150de7be046b1b9
Michal-Jackowski <michaljacko@op.pl>
2023-09-19 16:55:30 +0200

    README.md Update

--- ListingAnnouncementsScraper.py
+++ ListingAnnouncementsScraper.py
@@ -1,27 +1,30 @@
 from telethon.sync import TelegramClient
 import datetime
+import telegram_config
 #import pandas as pd
 
-api_id = ... # Insert api_id here
-api_hash = '...' # Insert api_hash 'here'
+# Insert api id here
+api_id = telegram_config.api_id
+# Insert api_hash here
+api_hash = telegram_config.api_hash
 
 #chats = ['...'] # Insert chats or channels here, legit one
-chats = ['...'] # Insert chats or channels here, just for testing purpose
+#chats = [telegram_config.user_input_channel]
+# Insert chats or channels here, just for testing purpose
+chats = [telegram_config.test_user_input_channel]
 
 client = TelegramClient('None', api_id, api_hash)
 
 #df = pd.DataFrame()
 
-print("Press ESC to Stop.")
 print("Waiting for Announcements...")
-
 while True:
     for chat in chats:
         with TelegramClient('test', api_id, api_hash) as client:
             for message in client.iter_messages(chat, offset_date=datetime.date.today(), reverse=True):
                 print(message.message)
-                print(message.date)
-                #print(message)
+                #print(message.date)
+                print(message)
                 #data = { "group" : chat, "sender" : message.sender_id, "text" : message.text, "date" : message.date}
                 #temp_df = pd.DataFrame(data, index=[1])
                 #df = df._append(temp_df)
