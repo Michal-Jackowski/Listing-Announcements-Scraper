6ac51c43ad4c4f8b754a62b156e86b6f1df74dd7
Michal-Jackowski <michaljacko@op.pl>
2023-09-20 18:46:30 +0200

    Scrape data from Telegram

--- ListingAnnouncementsScraper.py
+++ ListingAnnouncementsScraper.py
@@ -1,33 +1,26 @@
 from telethon.sync import TelegramClient
 import datetime
 import telegram_config
-#import pandas as pd
+import pandas as pd
+import path
 
-# Insert api id here
 api_id = telegram_config.api_id
-# Insert api_hash here
 api_hash = telegram_config.api_hash
-
-#chats = ['...'] # Insert chats or channels here, legit one
-#chats = [telegram_config.user_input_channel]
-# Insert chats or channels here, just for testing purpose
 chats = [telegram_config.test_user_input_channel]
 
-client = TelegramClient('None', api_id, api_hash)
+client = TelegramClient('Me', api_id, api_hash)
+df = pd.DataFrame()
 
-#df = pd.DataFrame()
-
-print("Waiting for Announcements...")
-while True:
-    for chat in chats:
-        with TelegramClient('test', api_id, api_hash) as client:
-            for message in client.iter_messages(chat, offset_date=datetime.date.today(), reverse=True):
-                print(message.message)
-                #print(message.date)
-                print(message)
-                #data = { "group" : chat, "sender" : message.sender_id, "text" : message.text, "date" : message.date}
-                #temp_df = pd.DataFrame(data, index=[1])
-                #df = df._append(temp_df)
+for chat in chats:
+    with TelegramClient('test', api_id, api_hash) as client:
+        #for message in client.iter_messages(chat, offset_date=datetime.date.today(), reverse=True):
+        for message in client.iter_messages(chat, reverse=True):
+            #print(message.message)
+            #print(message.date)
+            #print(message)
+            data = { "text" : message.text, "date" : message.date}
+            temp_df = pd.DataFrame(data, index=[1])
+            df = df._append(temp_df)
                 
-#df['date'] = df['date'].dt.tz_localize(None)
-#df.to_excel("...".format(datetime.date.today()), index=False)
+df['date'] = df['date'].dt.tz_localize(None)
+df.to_excel(path.scraped_data_from_telegram.format(datetime.date.today()), index=False)
---
+++ fix_asap.py
@@ -0,0 +1,2 @@
+# Only 100 rows from Telegram => API Limit
+# Data format in Excel is incorrect
