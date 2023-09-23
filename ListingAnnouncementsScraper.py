8cdd1a3ec496f72c2c481790c667b7245c42e31b
Michal-Jackowski <michaljacko@op.pl>
2023-09-23 19:33:55 +0200

    Auto Adjust Column Weight

--- ListingAnnouncementsScraper.py
+++ ListingAnnouncementsScraper.py
@@ -3,7 +3,6 @@ import datetime
 import telegram_config
 import pandas as pd
 import path
-import xlsxwriter
 
 api_id = telegram_config.api_id
 api_hash = telegram_config.api_hash
@@ -14,23 +13,20 @@ df = pd.DataFrame()
 
 for chat in chats:
     with TelegramClient('test', api_id, api_hash) as client:
-        #for message in client.iter_messages(chat, offset_date=datetime.date.today(), reverse=True):
         for message in client.iter_messages(chat, reverse=True):
-            #print(message.message)
-            #print(message.date)
-            #print(message)
-            #data = { "text" : message.text, "date" : message.date}
             data = {"date" : message.date, "text" : message.text,}
-            #df["date"] = pd.to_datetime(df["date"], format='%Y-%m-%d')
             temp_df = pd.DataFrame(data, index=[1])
-            #print(temp_df)
             df = df._append(temp_df)
                 
-#df['date'] = df['date'].dt.tz_localize(None)
 df['date'] = df['date'].dt.tz_localize(None)
-#df['text'] = pd.to_timedelta(df['text'])
-#writer = pd.ExcelWriter(path.scraped_data_from_telegram, engine='xlsxwriter', datetime_format='dd/mm/yy')
-#df.to_excel(writer, index=False)
-df.to_excel(path.scraped_data_from_telegram.format(datetime.date.today()), index=False)
+writer = pd.ExcelWriter(path.scraped_data_from_telegram.format(datetime.date.today()), engine='xlsxwriter')
+df.to_excel(writer, sheet_name="MySheet", index=False)
 
-#DaveX then you would do my_df[time_columns] = my_df[time_columns].dt.tz_localize(None) where time_columns is a list of column names that have datetime dtype
+workbook = writer.book
+worksheet = writer.sheets['MySheet']
+
+for i, col in enumerate(df.columns):
+    width = max(df[col].apply(lambda x: len(str(x))).max(), len(col))
+    worksheet.set_column(i, i, width)
+
+writer._save()
--- fix_asap.py
+++ fix_asap.py
@@ -1,2 +1,1 @@
 # Only 100 rows from Telegram => API Limit
-# Auto adjust column weight
